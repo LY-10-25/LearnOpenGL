76d4c828ded076ac1ed38aa265253c4318c90729
LinYouyang <56954464+LY-10-25@users.noreply.github.com>
2023-06-16 23:23:05 +0800

    test push

--- main.cpp
+++ main.cpp
@@ -164,12 +164,12 @@ int main()
     while (!glfwWindowShouldClose(window))
     {
         processInput(window);
-
+        std::cout << "runing..." << std::endl;
         glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT);
 
         glUseProgram(shaderProgram);
-            
+        
         glBindVertexArray(VAO);
         glDrawArrays(GL_TRIANGLES, 0, 3);
         glUseProgram(shaderProgram1);
