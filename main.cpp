764e1ba4fcad9c36dddea5c4ef3264e50630d95f
LinYouyang <56954464+LY-10-25@users.noreply.github.com>
2023-06-16 23:37:29 +0800

    test push 3

--- main.cpp
+++ main.cpp
@@ -164,9 +164,6 @@ int main()
     while (!glfwWindowShouldClose(window))
     {
         processInput(window);
-
-        std::cout << "running ..." << std::endl;
-
         glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT);
 
